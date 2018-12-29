5a00ed62c1042b140a5b0d9aa2e42d97604ca008
imclark <33160623+imclark@users.noreply.github.com>
2018-12-29 00:30:50 -0600

    interfaces

---
+++ Basics/interfaces/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type gopher struct {
+	name    string
+	age     int
+	isAdult bool
+}
+
+type horse struct {
+	name   string
+	weight int
+}
+
+type jumper interface {
+	jump() string
+}
+
+func (g gopher) jump() string {
+	if g.age < 65 {
+		return g.name + " can jump HIGH"
+	}
+	return g.name + " can still jump"
+}
+
+func (h horse) jump() string {
+	if h.weight > 2500 {
+		return "I'm too heavy, can't jump..."
+	}
+	return "I will jump, neigh!"
+}
+
+func main() {
+	jumperList := getList()
+	for _, jumper := range jumperList {
+		fmt.Println(jumper.jump())
+	}
+}
+
+func getList() []jumper {
+	phil := &gopher{name: "Phil", age: 20}
+	noodles := &gopher{name: "Noodles", age: 90}
+	barbaro := &horse{name: "Barbaro", weight: 2000}
+
+	list := []jumper{phil, noodles, barbaro}
+	return list
+}
